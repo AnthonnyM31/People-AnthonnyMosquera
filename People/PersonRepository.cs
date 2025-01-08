9f63c4f6ea29c77b9172b2426e6af19a128fa5fd
AnthonnyM31 <anthonnymosquera1482@gmail.com>
2025-01-07 19:07:54 -0500

    de modifica el metodo AddNewPerson para que este sea funcional

--- People/PersonRepository.cs
+++ People/PersonRepository.cs
@@ -29,22 +29,26 @@ public class PersonRepository
         int result = 0;
         try
         {
+            // se inicializa la base de datos por si acaso no lo esté
             Init();
 
-            // Basic validation to ensure a name was entered
+            // excecpción para controlar que se puso el nombre correcto
             if (string.IsNullOrEmpty(name))
                 throw new Exception("Valid name required");
 
-            // Insert the new person into the database
+            // se inserta el objeto en la base de datos
             result = conn.Insert(new Person { Name = name });
 
+            // y un mensaje de confirmación
             StatusMessage = string.Format("{0} record(s) added (Name: {1})", result, name);
         }
         catch (Exception ex)
         {
+           
             StatusMessage = string.Format("Failed to add {0}. Error: {1}", name, ex.Message);
         }
     }
+
 
     public List<Person> GetAllPeople()
     {
